d2f88c3d62f52a871afeabfb3a38138352b9304f
rsparks <rachel.sparks@kcl.ac.uk>
2024-12-17 18:29:44 +0000

    Puzzle Day 17 part 1 solution is not yet verified.

---
+++ Modules/PuzzleDay17.py
@@ -0,0 +1,119 @@
+import math
+
+from Modules.GenericPuzzle import Puzzle
+
+class register:
+    # the puzzle takes as input a filename
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.print = None
+    def combo(self, operand):
+        if operand == 4:
+            return self.a
+        elif operand == 5:
+            return self.b
+        elif operand == 6:
+            return self.c
+        else:
+            return operand
+
+    def stardv(self, operand):
+        return math.floor(self.a / pow(2, self.combo(operand)))
+
+    def toprint(self, val):
+        if self.print is None:
+            self.print = str(val)
+        else:
+            self.print+=',' + str(val)
+
+    # run the given instruction return -1 to indicate the operator should increment through the list by 2
+    # returns 0-N to indicate where the operand should jump to if requested
+    def run_instruction(self, opcode, operand):
+        match opcode:
+            # The adv instruction (opcode 0)  performs division. The numerator is the value in the A register.
+            # The denominator is found by raising 2 to the power of the instruction's combo operand. (So, an operand
+            # of 2 would divide A by 4 (2^2); an operand of 5 would divide A by 2^B.) The result of the division
+            # operation is truncated to an integer and then written to the A register
+            case 0:
+                self.a = self.stardv(operand)
+                return -1
+            #The bxl instruction (opcode 1) calculates the bitwise XOR of register B and the instruction's literal
+            # operand, then stores the result in register B.
+            case 1:
+                self.b = self.b^operand
+                return -1
+            #The bst instruction (opcode 2) calculates the value of its combo operand modulo 8
+            # (thereby keeping only its lowest 3 bits), then writes that value to the B register.
+            case 2:
+                self.b = self.combo(operand) % 8
+                return -1
+            # The jnz instruction (opcode 3) does nothing if the A register is 0. However, if the A register is not zero,
+            # it jumps by setting the instruction pointer to the value of its literal operand; if this instruction jumps,
+            # the instruction pointer is not increased by 2 after this instruction.
+            case 3:
+                if self.a == 0:
+                    return -1
+                else:
+                    return operand
+            #The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C, then stores the
+            # result in register B. (For legacy reasons, this instruction reads an operand but ignores it.)
+            case 4:
+                self.b = self.b^self.c
+                return -1
+            #The out instruction (opcode 5) calculates the value of its combo operand modulo 8, then outputs that value.
+            # (If a program outputs multiple values, they are separated by commas.)
+            case 5:
+                self.toprint(self.combo(operand) % 8)
+                return -1
+            #The bdv instruction (opcode 6) works exactly like the adv instruction except that the result is stored in
+            #  the B register. (The numerator is still read from the A register.)
+            case 6:
+                self.b = self.stardv(operand)
+                return -1
+            #The cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored in
+            # the C register. (The numerator is still read from the A register.)
+            case 7:
+                self.c = self.stardv(operand)
+                return -1
+
+class PuzzleDay17(Puzzle):
+    def __init__(self, filename):
+        super().__init__(filename)
+        self.register = None
+
+    def read_file(self):
+        f = open(self.filename,"r")
+        a, b, c = None, None, None
+
+        # for this file we have three parts
+        for line in f.readlines():
+            if "Register A:" in line:
+                a = int(line.split(": ")[1])
+            elif "Register B:" in line:
+                b = int(line.split(": ")[1])
+            elif "Register C:" in line:
+                c = int(line.split(": ")[1])
+            elif "Program: " in line: # we are reading the program instructions
+                self.input = line.rstrip().split(": ")[1]
+
+        self.register = register(a, b, c)
+        f.close()
+
+    def part1(self):
+        input_list = [int(item) for item in self.input.split(',')]
+        i = 0
+
+        while i < len(input_list):
+            j = self.register.run_instruction(input_list[i], input_list[i+1])
+            # increment appropriately
+            if j != -1:
+                i = j
+            else:
+                i += 2
+
+        print(self.register.print)
+
+    def part2(self):
+        pass
